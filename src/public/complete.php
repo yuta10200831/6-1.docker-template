4a9b2314eeb4180a814e2ee688ebc04dabb03335
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-08-24 23:25:54 +0900

    トップページ、投稿画面、DBの作成

---
+++ src/public/complete.php
@@ -0,0 +1,59 @@
+<?php
+$title = $_POST["title"];
+$email = $_POST["email"];
+$content = $_POST["content"];
+
+$errors = [];
+if (empty($title) || empty($email) || empty($content))
+{
+  $errors[] = '「タイトル」 「Email」 「お問い合わせ内容」のどれかが記入されておりません！';
+}
+
+  $dbUserName = 'root';
+  $dbPassword = 'password';
+  $pdo = new PDO(
+    'mysql:host=mysql; dbname=contactform; charset=utf8',
+    $dbUserName,
+    $dbPassword
+  );
+
+  $stmt = $pdo->prepare("INSERT INTO contacts (
+    title, email, content
+  ) VALUES (
+    :title, :email, :content
+  )");
+  
+  //登録するデータをセット
+  $stmt->bindParam( ':title', $title, PDO::PARAM_STR);
+  $stmt->bindParam( ':email', $email, PDO::PARAM_STR);
+  $stmt->bindParam( ':content', $content, PDO::PARAM_STR);
+  $res = $stmt->execute();
+  
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>お問い合わせ内容</title>
+</head>
+
+<body>
+  <div>
+    <?php if(!empty($errors)): ?>
+      <?php foreach($errors as $error): ?>
+        <p><?php echo $error . "<br>"; ?></p>
+      <?php endforeach; ?>
+      <a href="index.php">送信画面へ</a>
+    <?php endif; ?>
+
+    <?php if(empty($errors)): ?>
+      <h2>送信完了！！！！！！</h2>
+      <a href="index.php">送信画面へ</a><br><br>
+      <a href="">送信履歴へ</a>
+    <?php endif; ?>
+  </div>
+</body>
+    
+</html>
--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,25 @@
-<?php
-echo 'Welcome TECH QUEST!';
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>トップページ</title>
+    </head>
+    <body>
+    <h2>お問い合わせフォーム</h2>
+    <p>以下のフォームからお問い合わせください。</p>
+        <form action="complete.php" method="post">
+            タイトル(必須):
+            <input type="text" name="title" size="50" value="" /><br />
+
+            Email(必須):
+            <input type="text" name="email" size="50" value="" /><br />
+
+            お問い合せ内容(必須):
+            <textarea name="content" cols="50" rows="5"></textarea><br />
+
+            <br />
+
+            <input type="submit" value="送信" />
+        </form>
+    </body>
+</html>
